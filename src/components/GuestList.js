7ca41d1934acb917bb7f1dec094ebec64fcc5e09
Daniel Koller <daniel-koller@gmx.at>
2023-01-27 16:54:06 +0100

    Add function to show all the users

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+const baseUrl = 'http://localhost:4000';
 export default function GuestList() {
-  async function fetchUsers() {
-    const baseUrl = 'http://localhost:4000';
+  const [guestAPI, setGuestAPI] = useState([]);
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      setGuestAPI(allGuests);
+      console.log(allGuests);
+    }
 
-    const response = await fetch(`${baseUrl}/guests`);
-    const allGuests = await response.json();
-
-    console.log(allGuests);
-  }
-
-  fetchUsers();
-
-  return <div>GuestList</div>;
+    fetchUsers().catch((error) => console.log(error));
+  }, []);
+  return (
+    <div>
+      <h1>Names</h1>
+      {guestAPI.map((guest) => {
+        return (
+          <div key={guest.id}>
+            <div data-test-id="guest">
+              {guest.firstName} {guest.lastName}
+            </div>
+            <input type="checkbox" aria-label="" />
+            <button>Remove</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
