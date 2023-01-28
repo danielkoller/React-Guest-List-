1a514ec5822f925e39827f51107993a9c5377a6d
Daniel Koller <daniel-koller@gmx.at>
2023-01-28 11:41:56 +0100

    Add function to delete user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,91 @@
-import GuestForm from './components/GuestForm';
-import GuestList from './components/GuestList';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [guestAPI, setGuestAPI] = useState([]);
+  const baseUrl = 'http://localhost:4000';
+
+  // Enter the Guests
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ firstName: firstName, lastName: lastName }),
+    });
+    const createdGuest = await response.json();
+    const newGuest = { firstName, lastName };
+    setFirstName('');
+    setLastName('');
+    console.log(createdGuest);
+    console.log(newGuest);
+  }
+
+  // Delete a guest
+
+  async function deleteGuest() {
+    await fetch(`${baseUrl}/guests/${guestAPI[0]['id']}`, {
+      method: 'DELETE',
+    });
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+    setGuestAPI(allGuests);
+  }
+
+  // Show the Guests
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      setGuestAPI(allGuests);
+      console.log(allGuests);
+    }
+
+    fetchUsers().catch((error) => console.log(error));
+  }, [firstName]);
+
   return (
     <div>
       <h1>Guest List</h1>
-      <GuestForm />
-      <GuestList />
+      <form onSubmit={handleSubmit}>
+        <label id="firstName" htmlFor="firstName">
+          First Name:
+          <input
+            value={firstName}
+            placeholder="First Name"
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+        <label id="lastName" htmlFor="lastName">
+          Last Name:
+          <input
+            value={lastName}
+            placeholder="Last Name"
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+        <p>Press Enter to submit!</p>
+        <button>Submit</button>
+      </form>
+      <div>
+        <h2>Names</h2>
+        {guestAPI.map((guest) => {
+          return (
+            <div key={guest.id}>
+              <div data-test-id="guest">
+                {guest.firstName} {guest.lastName}
+              </div>
+              <input type="checkbox" aria-label="" />
+              <button onClick={deleteGuest}>Remove</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
